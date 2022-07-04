76b5fd4013887d567f031c4a41ed985bc256b3b4
유연수 <ash.you.dev@gmail.com>
2022-07-04 23:14:14 +0900

    STYLE: STEP1 Exercise 타입 반환 키워드 및 타입 생략, 단축 인자이름, 후행클로저로 수정

--- CodeStarterCamp_Week4/CodeStarterCamp_Week4/exercise.swift
+++ CodeStarterCamp_Week4/CodeStarterCamp_Week4/exercise.swift
@@ -12,30 +12,30 @@ struct Exercise {
     let action: (_ bodyCondition: BodyCondition) -> Void
 }
 
-let 윗몸일으키기: Exercise = Exercise(name: "윗몸일으키기", action: { (_ bodyCondition: BodyCondition) in
-    bodyCondition.상체근력 += Int.random(in: 10...20)
-    bodyCondition.피로도 += Int.random(in: 10...20)
-})
+let 윗몸일으키기: Exercise = Exercise(name: "윗몸일으키기") {
+    $0.상체근력 += Int.random(in: 10...20)
+    $0.피로도 += Int.random(in: 10...20)
+}
 
-let 스쿼트: Exercise = Exercise(name: "스쿼트", action: { (_ bodyCondition: BodyCondition) in
-    bodyCondition.하체근력 += Int.random(in: 20...30)
-    bodyCondition.피로도 += Int.random(in: 10...20)
-})
+let 스쿼트: Exercise = Exercise(name: "스쿼트") {
+    $0.하체근력 += Int.random(in: 20...30)
+    $0.피로도 += Int.random(in: 10...20)
+}
 
-let 오래달리기: Exercise = Exercise(name: "오래달리기", action: { (_ bodyCondition: BodyCondition) in
-    bodyCondition.근지구력 += Int.random(in: 20...30)
-    bodyCondition.상체근력 += Int.random(in: 5...10)
-    bodyCondition.하체근력 += Int.random(in: 5...10)
-    bodyCondition.피로도 += Int.random(in: 20...30)
-})
+let 오래달리기: Exercise = Exercise(name: "오래달리기") {
+    $0.근지구력 += Int.random(in: 20...30)
+    $0.상체근력 += Int.random(in: 5...10)
+    $0.하체근력 += Int.random(in: 5...10)
+    $0.피로도 += Int.random(in: 20...30)
+}
 
-let 동적휴식: Exercise = Exercise(name: "동적휴식", action: { (_ bodyCondition: BodyCondition) in
-    bodyCondition.피로도 -= Int.random(in: 5...10)
-})
+let 동적휴식: Exercise = Exercise(name: "동적휴식") {
+    $0.피로도 -= Int.random(in: 5...10)
+}
 
-let 새천년체조: Exercise = Exercise(name: "새천년체조", action: { (_ bodyCondition: BodyCondition) in
-    bodyCondition.근지구력 += Int.random(in: 1...5)
-    bodyCondition.상체근력 += Int.random(in: 1...5)
-    bodyCondition.하체근력 += Int.random(in: 1...5)
-    bodyCondition.피로도 += Int.random(in: 1...5)
-})
+let 새천년체조: Exercise = Exercise(name: "새천년체조") {
+    $0.근지구력 += Int.random(in: 1...5)
+    $0.상체근력 += Int.random(in: 1...5)
+    $0.하체근력 += Int.random(in: 1...5)
+    $0.피로도 += Int.random(in: 1...5)
+}
